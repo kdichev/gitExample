ac6501737c8eea627c618d4b54ba960878f374dc
Simeon Arabov <simeon.arabov@kcl.ac.uk>
2017-07-11 17:45:16 +0100

    add comments

--- car.js
+++ car.js
@@ -1,13 +1,15 @@
-var Car ,prius;
-
+var Car ,prius; //RIP VANKS NEW CAR
+//LOOOOL VANKTA 160km/h
 Car = function(b, m, y) {
   this.brand = b;
   this.model = m;
   this.year = y;
 };
+//KOKO BOSS
+//VANKATA NO CAR
 
 Car.prototype.rent = function() {
   console.log("You've just rent a " + this.model + ".");
 };
-
+//RIP VANKATA CAR
 prius = new Car("Toyota", "Prius", "2012");
