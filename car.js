d15e3ad57bb196861ef5970099c2dab786928fb2
Konstantin-home <konstantin.dichev@gmail.com>
2017-07-11 19:18:14 +0200

    commit directly to master :fire: never do this

--- car.js
+++ car.js
@@ -1,8 +1,9 @@
 let Car;
 let prius;
 //LOOOOL VANKTA 160km/h
-Car = function(b, m, y) {
+Car = function(b, m, y, /*asd*/) {
   this.brand = b;
+  // asd
   this.model = m;
   this.year = y;
 };
@@ -10,9 +11,11 @@ Car = function(b, m, y) {
 //VANKATA NO CAR
 
 Car.prototype.rent = function() {
+  // asd
   console.log("You've just rent a " + this.model + ".");
 };
 //RIP VANKATA CAR
 prius = new Car("Toyota", "Prius", "2012");
 
 console.log("gui" + prius);
+//asd
