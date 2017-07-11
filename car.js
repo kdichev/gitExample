56b48a4ad9543bbc5e5c6cf4dd1053a628b9f28f
Konstantin-home <konstantin.dichev@gmail.com>
2017-07-11 19:22:04 +0200

    add this amazing new feature

--- car.js
+++ car.js
@@ -1,7 +1,7 @@
 let Car;
 let prius;
 //LOOOOL VANKTA 160km/h
-Car = function(b, m, y) {
+Car = (b, m, y) => {
   this.brand = b;
   this.model = m;
   this.year = y;
@@ -9,10 +9,22 @@ Car = function(b, m, y) {
 //KOKO BOSS
 //VANKATA NO CAR
 
-Car.prototype.rent = function() {
+Car.prototype.rent = () => {
   console.log("You've just rent a " + this.model + ".");
+};
+
+Car.prototype.return = () => {
+  console.log("You've just returned" + this.model + ".");
 };
 //RIP VANKATA CAR
 prius = new Car("Toyota", "Prius", "2012");
+angelaMergel = new Car("Mercedess", "C-Klass", "2009");
 
 console.log("gui" + prius);
+console.log("gui" + prius.rent());
+console.log("gui" + prius.return());
+console.log("TEJKOTO STUPVANE", angelaMergel);
+console.log("TEJKOTO STUPVANE", angelaMergel);
+console.log("TEJKOTO STUPVANE", angelaMergel);
+console.log("TEJKOTO STUPVANE", angelaMergel);
+console.log("TEJKOTO STUPVANE", angelaMergel);
